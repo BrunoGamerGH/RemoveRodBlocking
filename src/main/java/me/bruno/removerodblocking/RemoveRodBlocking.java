4988b5226ff7e62ade6adf756040915203a48464
BrunoGamerGH <bruno.manarino@gmail.com>
2023-10-28 14:59:18 -0300

    change method of rendering & show rod line

--- src/main/java/me/bruno/removerodblocking/RemoveRodBlocking.java
+++ src/main/java/me/bruno/removerodblocking/RemoveRodBlocking.java
@@ -1,8 +1,12 @@
 package me.bruno.removerodblocking;
 
+import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
 
 public class RemoveRodBlocking implements ClientModInitializer {
+
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     private static RemoveRodBlocking instance;
 
