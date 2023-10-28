37488dbc69674648c8c5b9d249ee3adf244042e2
BrunoGamerGH <bruno.manarino@gmail.com>
2023-10-28 19:50:41 -0300

    prevent crash from happening

--- src/main/java/me/bruno/removerodblocking/mixin/FishingBobberEntityRendererMixin.java
+++ src/main/java/me/bruno/removerodblocking/mixin/FishingBobberEntityRendererMixin.java
@@ -24,6 +24,9 @@ import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FishingBobberEntityRenderer.class)
 public abstract class FishingBobberEntityRendererMixin extends EntityRenderer<FishingBobberEntity> {
@@ -72,8 +75,8 @@ public abstract class FishingBobberEntityRendererMixin extends EntityRenderer<Fi
         stack.pop();
     }
 
-    @Override
-    public void render(FishingBobberEntity fishingBobberEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+    @Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/entity/projectile/FishingBobberEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", cancellable = true)
+    public void renderExceptInHead(FishingBobberEntity fishingBobberEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
         double s;
         float r;
         double q;
@@ -137,5 +140,6 @@ public abstract class FishingBobberEntityRendererMixin extends EntityRenderer<Fi
         }
         matrixStack.pop();
         super.render(fishingBobberEntity, f, g, matrixStack, vertexConsumerProvider, i);
+        ci.cancel();
     }
 }
